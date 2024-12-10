91b746c3be2c705377f5fea456a87cb890431fac
Josh Reep <joshreep@gmail.com>
2024-12-09 22:27:36 -0500

    feat(utils): add error handling utils

---
+++ src/utils/error-handling.ts
@@ -0,0 +1,14 @@
+import { PrismaClientKnownRequestError, PrismaClientValidationError } from "@prisma/client/runtime/library";
+
+export function getStatusCodeFromError(error: unknown) {
+    if (error instanceof PrismaClientValidationError) return 400
+    if (error instanceof PrismaClientKnownRequestError) return 404
+    else return 500
+}
+
+export function getErrorMessageFromError(error: unknown) {
+    if (error instanceof PrismaClientValidationError) return error.message
+    if (error instanceof PrismaClientKnownRequestError) return error.meta?.cause
+    else return 'Something went wrong with the request'
+}
+
